aa2cc14d2b93fa3a3f352eb0208304645ed4aea2
Connor Fricke <cd.fricke23@gmail.com>
2025-02-20 11:28:22 -0500

    updated name, added monteCarlo integration routine

---
+++ Calculus.cpp
@@ -0,0 +1,51 @@
+/*
+Programmer: Connor Fricke (cd.fricke23@gmail.com)
+File: Calculus.h
+Latest Revision: 19-Feb-2024
+Synopsis: Implementation File for Integration functions
+*/
+
+#include <cstdlib>
+#include "Calculus.h"
+
+using namespace calculus;
+
+double integral::trapezoidal(double (*func)(double), const double a, const double b, const int n)
+{
+    double h = ((b - a)/double(n - 1)); // point spacing
+    double sum= 0.0;
+
+    // midpoint contributions
+    for (int i=2; i<n; i++) 
+    { 
+        double x = a + h * double(i-1);
+        sum += h * func(x);
+    }
+    // endpoint contributions
+    sum += (h/2.0) * (func(a) + func(b));
+
+    return (sum);   
+}
+
+double integral::monteCarlo(double (*func)(double), const double a, const double b, const int iterations)
+{
+    /* 
+    https://cameron-mcelfresh.medium.com/monte-carlo-integration-313b37157852
+    */
+
+	double sum = 0;
+	double randNum, f;
+
+	int iter = 0;
+
+    for (int i = 0; i < iterations - 1; i++)
+	{
+		// generate random number within bounds
+		randNum = a + (float(std::rand())/RAND_MAX) * (b - a);
+        // sample function
+		f = func(randNum);
+		sum += f;
+	}
+
+	return (b - a) * sum / iterations;
+}
---
+++ Calculus.h
@@ -0,0 +1,27 @@
+/*
+Programmer: Connor Fricke (cd.fricke23@gmail.com)
+File: Calculus.h
+Latest Revision: 19-Feb-2024
+Synopsis: Header File for Integration function library
+*/
+
+#ifndef CALCULUS_H
+#define CALCULUS_H
+
+    namespace calculus::integral {
+
+        /* Adapted from Physics 5810 with Prof. Ralf Bundschuh and Prof. Dick Furnstahl */
+        double trapezoidal(double (*func)(double), const double a, const double b, const int n);
+        
+        /* Adapted from adapted from Cameron McElfresh */
+        /* https://cameron-mcelfresh.medium.com/monte-carlo-integration-313b37157852 */
+        double monteCarlo(double (*func)(double), const double a, const double b, const int n);
+        
+    }
+
+    namespace calculus::derivative {
+
+        
+    }
+
+#endif
