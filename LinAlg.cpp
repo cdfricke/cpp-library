49c5b308d1027343c4f7ccccb0b63acc43803507
Connor Fricke <cd.fricke23@gmail.com>
2025-02-22 17:31:29 -0500

    added LinAlg.cpp, LinAlg.h for future implementation

---
+++ LinAlg.cpp
@@ -0,0 +1,8 @@
+/*
+Programmer: Connor Fricke (cd.fricke23@gmail.com)
+File: LinAlg.cpp
+Latest Revision: 22-Feb-2024
+Synopsis: Implementation File for linear algebra routines
+*/
+
+#include "LinAlg.h"
---
+++ LinAlg.h
@@ -0,0 +1,10 @@
+/*
+Programmer: Connor Fricke (cd.fricke23@gmail.com)
+File: LinAlg.h
+Latest Revision: 22-Feb-2024
+Synopsis: Header File for linear algebra routines
+*/
+
+#include <cstdlib>
+#include <vector>
+
